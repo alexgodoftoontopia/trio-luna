f9cb5a6577e14d68f2a537f03496851c6366ddce
alexgodoftoontopia <mrwaltdisneyclassicisback@gmail.com>
2025-07-15 23:23:05 +0200

    Update episodes.js

--- src/data/episodes.js
+++ src/data/episodes.js
@@ -22,7 +22,13 @@ const episodes = [
       { number: 11, title: "Trio Luna Meets The Little Mermaid" },
       { number: 12, title: "Trio Luna Meets The Lion King" },
       { number: 13, title: "Trio Luna Meets Bambi" },
-      { number: 14, title: "Trio Luna Meets The Jungle Book" }
+      { number: 14, title: "Trio Luna Meets The Jungle Book" },
+      { number: 15, title: "Trio Luna Meets The Aristocats" },
+      { number: 16, title: "Trio Luna Meets The Fox and the Hound" },
+      { number: 17, title: "Trio Luna Meets The Rescuers" },
+      { number: 18, title: "Trio Luna Meets The Sword in the Stone" },
+      { number: 19, title: "Trio Luna Meets The Emperor's New Groove" },
+      { number: 20, title: "Trio Luna Meets Mulan" }
       // episodios de la temporada 2
     ]
   }
