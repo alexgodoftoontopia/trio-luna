796c06067ef5cf0d16b72dece96242550bb3b044
alexgodoftoontopia <mrwaltdisneyclassicisback@gmail.com>
2025-07-19 23:24:29 +0200

    Update episodes.js

--- src/data/episodes.js
+++ src/data/episodes.js
@@ -3,16 +3,16 @@ const rawEpisodes = [
     season: 1,
     title: "Temporada 1",
     episodes: [
-      { title: "Trio Luna's Adventures of The Little Mermaid: Ariel's Beginning" },
-      { title: "Trio Luna Meets The Prince and The Pauper" },
-      { title: "Trio Luna Knows How the Grinch Stole Christmas" },
+      { title: "Trio Luna's Adventures of Fantasia" },
+      { title: "Trio Luna Meets The Reluctant Dragon" },
+      { title: "Trio Luna Meets Dumbo" },
+      { title: "Trio Luna Meets Bambi" },
+      { title: "Trio Luna's Adventures of Mickey and the Beanstalk" },
       { title: "Trio Luna Meets Pocahontas"},
-      { title: "Trio Luna Meets Ferdinand"},
-      { title: "Trio Luna's Adventures of The Nightmare Before Christmas" },
-      { title: "Trio Luna Meets The Hunchback of Notre Dame" },
-      { title: "Trio Luna Meets Oliver and Company" },
-      { title: "Trio Luna Meets Dumbo"},
-      { title: "Trio Luna's Adventures of Fantasia"}
+      { title: "Trio Luna Meets The Little Mermaid: Ariel's Beginning" },
+      { title: "Trio Luna's Adventures ofThe Jungle Book" },
+      { title: "Trio Luna Meets The Lion King" },
+      { title: "Trio Luna Meets Tarzan"}
       // episodios de la temporada 1
     ]
   },
@@ -21,15 +21,16 @@ const rawEpisodes = [
     title: "Temporada 2",
     episodes: [
       { title: "Trio Luna Meets The Little Mermaid" },
-      { title: "Trio Luna Meets The Lion King" },
-      { title: "Trio Luna Meets Bambi" },
-      { title: "Trio Luna's Adventures of The Jungle Book" },
+      { title: "Trio Luna Meets The Prince and the Pauper" },
+      { title: "Trio Luna Meets Aladdin" },
+      { title: "Trio Luna Meets Hercules" },
+      { title: "Trio Luna Meets The Hunchback of Notre Dame" },
       { title: "Trio Luna Meets The Aristocats" },
       { title: "Trio Luna Meets The Fox and the Hound" },
       { title: "Trio Luna Joins to The Rescuers" },
       { title: "Trio Luna and The Sword in the Stone" },
       { title: "Trio Luna and The Emperor's New Groove" },
-      { title: "Trio Luna Meets Mulan" }
+      { title: "Trio Luna Meets Mulan" } 
       // episodios de la temporada 2
     ]
   }
