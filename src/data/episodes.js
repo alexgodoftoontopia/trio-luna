1168a0e3ab47d010ea02db31703b0a3c493ded5d
alexgodoftoontopia <mrwaltdisneyclassicisback@gmail.com>
2025-07-20 17:22:37 +0200

    Update episodes.js

--- src/data/episodes.js
+++ src/data/episodes.js
@@ -3,16 +3,12 @@ const rawEpisodes = [
     season: 1,
     title: "Temporada 1",
     episodes: [
+      { title: "Trio Luna Meets Snow White and the Seven Dwarfs" },
+      { title: "Trio Luna Meets Pinocchio" },
       { title: "Trio Luna's Adventures of Fantasia" },
       { title: "Trio Luna Meets The Reluctant Dragon" },
       { title: "Trio Luna Meets Dumbo" },
       { title: "Trio Luna Meets Bambi" },
-      { title: "Trio Luna's Adventures of Mickey and the Beanstalk" },
-      { title: "Trio Luna Meets Pocahontas"},
-      { title: "Trio Luna Meets The Little Mermaid: Ariel's Beginning" },
-      { title: "Trio Luna's Adventures ofThe Jungle Book" },
-      { title: "Trio Luna Meets The Lion King" },
-      { title: "Trio Luna Meets Tarzan"}
       // episodios de la temporada 1
     ]
   },
@@ -20,18 +16,26 @@ const rawEpisodes = [
     season: 2,
     title: "Temporada 2",
     episodes: [
-      { title: "Trio Luna Meets The Little Mermaid" },
-      { title: "Trio Luna Meets The Prince and the Pauper" },
-      { title: "Trio Luna Meets Aladdin" },
-      { title: "Trio Luna Meets Hercules" },
-      { title: "Trio Luna Meets The Hunchback of Notre Dame" },
-      { title: "Trio Luna Meets The Aristocats" },
-      { title: "Trio Luna Meets The Fox and the Hound" },
-      { title: "Trio Luna Joins to The Rescuers" },
-      { title: "Trio Luna and The Sword in the Stone" },
-      { title: "Trio Luna and The Emperor's New Groove" },
-      { title: "Trio Luna Meets Mulan" } 
+      { title: "Trio Luna's Adventures of Saludos Amigos'" },
+      { title: "Trio Luna Meets The Three Caballeros" },
+      { title: "Trio Luna Make Mine Music" },
+      { title: "Trio LUna's Adventures of Fun and Fancy Free" },
+      { title: "Trio Luna's Adventures of Melody Time" },
+      { title: "Trio Luna's Adventures of The Adventures of Ichabod and Mr. Toad" }
       // episodios de la temporada 2
+    ]
+  },
+  {
+    season: 3,
+    title: "Temporada 3",
+    episodes: [
+      { title: "Trio Luna Meets Cinderella" },
+      { title: "Trio Luna Meets Alice in Wonderland" },
+      { title: "Trio Luna Meets Peter Pan" },
+      { title: "Trio Luna Meets Lady and the Tramp" },
+      { title: "Trio Luna Meets Sleeping Beauty" },
+      { title: "Trio Luna Meets 101 Dalmatians" }
+      // episodios de la temporada 3
     ]
   }
 ];
