499795844cde488d82b2e6eeefde2901d0381fe2
alexgodoftoontopia <mrwaltdisneyclassicisback@gmail.com>
2025-08-08 23:37:54 +0200

    Update episodes.js

--- src/data/episodes.js
+++ src/data/episodes.js
@@ -4,9 +4,11 @@ const rawEpisodes = [
     title: "Temporada 1",
     episodes: [
       { title: "Trio Luna's Adventures of We're Back a Dinosaur Story" },
+      { title: "Trio Luna's Adventures of The Land Before Time" },
+      { title: "Trio Luna Meets The Great Mouse Detective" },
       { title: "Trio Luna's Adventures of Pokemon the Movie: Secrets of the Jungle" },
       { title: "Trio Luna's Adventures of The Secret of NIHM" },
-      { title: "Trio Luna Goes to Hotel Transylvania" },
+      { title: "Trio Luna Meets The Prince of Egypt" },
       { title: "Trio Luna's Adventures of Babes in Toyland"},
       { title: "Trio Luna's Adventures of Transformers One" },
       { title: "Trio Luna's Adventures of X-Men: The Last Stand" },
