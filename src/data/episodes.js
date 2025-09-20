f0125fff0d12273945f0a410e392af798dd3a498
alexgodoftoontopia <mrwaltdisneyclassicisback@gmail.com>
2025-09-20 16:38:15 +0200

    Update episodes.js

--- src/data/episodes.js
+++ src/data/episodes.js
@@ -17,7 +17,6 @@ const rawEpisodes = [
       { title: "Trio Luna Meets Paul Bunyan" },
       { title: "Trio Luna Meets The Prince and the Pauper" },
       { title: "Trio Luna's Adventures of Fantasia" }
-      // episodios de la temporada 1
     ]
   },
 
@@ -26,22 +25,19 @@ const rawEpisodes = [
     title: "Temporada 2",
     episodes: [
       { title: "Trio Luna Meets Dumbo" },
-      {title: "Trio Luna's Adventures of Donkey Kong Country: The Legend of the Crystal Coconut"},
-      { title: "Trio Luna's Adventures of Pokemon: The Firts Movie"},
-      { title: "Trio Luna's Adventures of The Lion King"},
+      { title: "Trio Luna's Adventures of Donkey Kong Country: The Legend of the Crystal Coconut" },
+      { title: "Trio Luna's Adventures of Pokemon: The Firts Movie" },
+      { title: "Trio Luna's Adventures of The Lion King" },
       { title: "Trio Luna Meets Pocahontas" },
-      { title: "Trio Luna's Adventures of The Little Mermaid: Ariel's Beginning"},
-      { title: "Trio Luna Meets The Hunchback of Notre Dame"},
-      { title: "Trio Luna's Adventures of Beauty and the Beast"},
-      { title: "Trio Luna Meets The Last Unicorn"},
-      { title: "Trio Luna and The Flight of Dragons"}
-      // episodios de la temporada 2
+      { title: "Trio Luna's Adventures of The Little Mermaid: Ariel's Beginning" },
+      { title: "Trio Luna Meets The Hunchback of Notre Dame" },
+      { title: "Trio Luna's Adventures of Beauty and the Beast" },
+      { title: "Trio Luna Meets The Last Unicorn" },
+      { title: "Trio Luna and The Flight of Dragons" }
     ]
   }
-
 ];
 
-// Asignar numeración automática
 const episodes = rawEpisodes.map(season => {
   let episodeCounter = 1; // Reinicia el contador en cada temporada
   return {
