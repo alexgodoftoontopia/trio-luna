f75b0153fe3c8a7f769403d545ea8a7693a8bb06
alexgodoftoontopia <mrwaltdisneyclassicisback@gmail.com>
2025-09-27 01:24:11 +0200

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Header() {
   return (
     <header>
-      <h1>Trio Luna's Adventures Series</h1>
+      <h1>Trio Luna's Adventures Universe</h1>
     </header>
   );
 }
