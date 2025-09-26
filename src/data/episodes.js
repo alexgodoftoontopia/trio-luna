f187c52cd0dab2253b1dd2511885e382689b2e51
alexgodoftoontopia <mrwaltdisneyclassicisback@gmail.com>
2025-09-27 01:17:00 +0200

    Update episode data and site title
    Changed the HTML title to 'Trio Luna's Adventures Universe'. Refactored episodes.js to reorganize and expand episode series, adding new seasons and updating episode titles for better clarity and structure.

--- src/data/episodes.js
+++ src/data/episodes.js
@@ -1,34 +1,65 @@
 const rawEpisodes = [
   {
     season: 1,
-    title: "Temporada 1",
+    title: "Trio Luna's Adventures Series",
     episodes: [
       { title: "Trio Luna in Once Upon a Studio" },
       { title: "Trio Luna wants to Wish" },
       { title: "Trio Luna Meets Talon" },
-      { title: "Trio Luna Meets Dumbo" },
       { title: "Trio Luna Meets Aladdin" },
-      { title: "Trio Luna's Adventures of The Super Mario Bros Movie" },
-      { title: "Trio Luna Meets Bambi" },
       { title: "Trio Luna Meets The Little Mermaid" },
       { title: "Trio Luna's Adventures of Fantasia" },
       { title: "Trio Luna's Nightmare Before Christmas" },
       { title: "Trio Luna's Adventures of The Lion King" },
-      { title: "Trio Luna Meets Pocahontas" },
       { title: "Trio Luna Meets The Hunchback of Notre Dame" },
       { title: "Trio Luna's Adventures of Beauty and the Beast" },
-      { title: "Trio Luna Meets The Last Unicorn" },
-      { title: "Trio Luna and The Flight of Dragons" }
+      { title: "Trio Luna and The Flight of Dragons"}
     ]
   },
 
   {
-    season: 1,
-    title: "Temporada Children of the Night's Adventures",
+    season: 2,
+    title: "Trio Guardians Adventures Series",
     episodes: [
-     { title: "Children of Nights joins Gulliver's Travels" }, //Esta aventura sucede después de Fantasia
-    { title: "Children of Nights Meets The Brave Little Toaster" },
-    { title: "Trio Luna and The Brave Little Toaster to the Rescue" }
+    { title: "Trio Guardians Meets Bambi" },  
+    { title: "Trio Guardians Adventures of The Super Mario Bros Movie" },
+    { title: "Trio Guardians Meets Pocahontas" },
+    { title: "Trio Guardians Meets Anastasia"}
+    ]
+  },
+
+  {
+    season: 3,
+    title: "Bambi and Cream's Adventures Series",
+    episodes: [
+    { title: "Bambi and Cream Meets Dumbo" }, //Esta aventura sucede después de Fantasia
+    { title: "Bambi and Cream Meets The Reluctant Dragon"},
+    { title: "Bambi and Cream's Adventures of Pokemon I Choose You"},
+    { title: "Bambi and Cream joins Gulliver's Travels" }, 
+    { title: "Bambi and Cream Meets The Brave Little Toaster" },
+    { title: "Bambi, Cream and The Brave Little Toaster to the Rescue" }
+      
+    ]
+  },
+
+   {
+    season: 4,
+    title: "Alex and Kaa's Adventures Series",
+    episodes: [
+    { title: "Alex and Kaa's Adventures of Donkey Kong Country" },
+    { title: "Bambi and Cream's Adventures of Pokemon I Choose You"},
+    { title: "Bambi and Cream joins Gulliver's Travels" }, 
+    { title: "Bambi and Cream Meets The Brave Little Toaster" },
+    { title: "Bambi, Cream and The Brave Little Toaster to the Rescue" }
+      
+    ]
+  },
+
+  {
+    season: 5,
+    title: "Crossovers Adventures Series",
+    episodes: [
+    { title: "Trio Luna and Trio Guardians Meets The Last Unicorn" }
       
     ]
   }
