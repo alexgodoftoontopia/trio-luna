4a633be6bb43e056aeda22b3250771dfae195a9c
alexgodoftoontopia <mrwaltdisneyclassicisback@gmail.com>
2025-09-27 13:44:27 +0200

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,6 +4,7 @@ function Footer() {
   return (
     <footer>
       <p>&copy; 2025 Trio Luna Adventures Series</p>
+      <p><a href="https://alexgodoftoontopia.github.io/toontopia-cinematic-universe/index.html">Inicio</a>" </p>
     </footer>
   );
 }
