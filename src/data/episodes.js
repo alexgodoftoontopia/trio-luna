e8e379e945af2e6ddecde6ae5b09892d10d30caf
alexgodoftoontopia <mrwaltdisneyclassicisback@gmail.com>
2025-09-27 12:13:42 +0200

    Update episodes.js

--- src/data/episodes.js
+++ src/data/episodes.js
@@ -6,8 +6,8 @@ const rawEpisodes = [
       { title: "Trio Luna in Once Upon a Studio" },
       { title: "Trio Luna Meets Talon" }, //Talon se unira al final del piloto
       { title: "Trio Luna wants to Wish" }, //Debut de Talon y Estrella se une al final, El trio lunatica (Opaline, Ash y Susie) se aliaran con el Rey Magnifico, Black Hat aparecera en el post credito como Thanos
-      {title: "Trio Luna's Adventures of The Jungle Book"}, //debut de Estrella y adoptan a Jake y Jenna (los hijos de Soarin), Rey Sombra, Magica y Bill Cipher se aliaran con Shere Khan
-      { title: "Trio Luna Meets Aladdin" }, //el trio lunatica (Opaline, Ash y Susie) se aliaran con Jafar
+      {title: "Trio Luna's Adventures of The Jungle Book"}, //debut de Estrella y adoptan a Jake y Jenna (los hijos de Soarin), el trio lunatica se aliaran con Shere Khan, en el post credito Black Hat invoca a Rey Sombra, Magica De hechizos, Bill Cipher y Lord Dominador haciendo referencia a My Little Pony
+      { title: "Trio Luna Meets Aladdin" }, //Rey Sombra, Magica De hechizos, Bill Cipher y Lord Dominador se aliaran con Jafar
       { title: "Trio Luna Meets The Little Mermaid" },
       { title: "Trio Luna's Adventures of Fantasia" },
       { title: "Trio Luna's Nightmare Before Christmas" },
